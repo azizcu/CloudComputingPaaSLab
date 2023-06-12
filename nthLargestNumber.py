a8aa79ab71bc13f028f6f4f124f088baf0ba39ec
azizcu <63375421+azizcu@users.noreply.github.com>
2023-06-12 20:25:48 +0600

    Update nthLargestNumber.py

--- nthLargestNumber.py
+++ nthLargestNumber.py
@@ -10,11 +10,11 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     # List of Integers
-    numbers = [1, 3, 4, 2]
+    numbers = [10, 23, 44, 25]
  
     #Sorting list of Integers
     numbers.sort()
-    return "2nd largest number from the given list is "+ str(numbers[len(numbers)-2])
+    return "Second largest number of list is: "+ str(numbers[len(numbers)-2])
 
 
 if __name__ == '__main__':
